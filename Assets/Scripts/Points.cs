7305be570a80790d9f7554d8d5d992a90005e38e
Pim Horeman <pimhoreman@gmail.com>
2015-11-11 21:03:24 +0100

    ResourcePoints Script (Almost Done)

---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Points : MonoBehaviour {
+
+	private int score = 0;
+
+	[SerializeField]
+	private GameObject gameobject;
+	private int increase = 20;
+	
+	void Awake () {
+		InvokeRepeating("AddScore", 0, 3);
+	}
+	void AddScore () {
+		score += increase;
+
+		Debug.Log ("Total: " + score);
+
+
+	}
+
+}
