da96ea11ea98421cc0cd353601f42df55ed87f30
Pim Horeman <pimhoreman@gmail.com>
2015-11-16 18:11:08 +0100

    Pathfinding 1/3, Cell Finished

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+		private float _g			= 0; 		// de g score is de score voor het pad van het beginpunt tot deze cell. Deze score hebben we berekent
+		private float _h			= 0; 		// de h score is de score voor het pad die we schatten vanaf deze cell tot het eindpunt.
+		private float _f			= 0; 		// f = g + h; de variabele f is de geschatte score van het beginpunt VIA deze cell naar het eindpunt
+		private bool _isWall	=	false;		// boolean om deze cell als wall te definieren
+		private bool _isOpen 	=	false; 	// variabele om (snel) bij te houden of we deze cell als buurman hebben gezien en nog moeten checken
+		private bool _isClosed	=	false; 	// variabele om bij te houden of we deze cell al hebben gecheckt
+		private	Cell _parent; 				// variabele om bij te houden wie, in het pad, mijn parent is (via welke cell kom je bij mij)
+		private Vector2 _position;			// wat is mijn positie in het grid
+		private ArrayList _neighbors; 			// wie zijn mijn buren (cellen)
+		
+		
+		public void Start() 
+		{
+			_position	=	new Vector2(1, 1);
+		}
+		
+		
+		public Vector2 position 
+		{
+			get { return _position;}
+			set	{ _position = value;}
+		}
+
+		public float f 
+		{
+			get	{ return _f; }
+			set {_f = value; }
+		}
+		
+		public float g
+		{
+			get	{ return _g; }
+			set {_g = value; }
+		}
+		
+		public float h
+		{
+			get	{ return _h; }
+			set {_h = value; }
+		}
+		
+		public bool Walls
+		{
+			get	{ return _isWall; }
+			set {_isWall = value; }
+		}
+		
+		public Cell parent
+		{
+			get	{ return _parent; }
+			set {_parent = value; }
+		}
+		
+		public bool isOpen 
+		{
+			get	{ return _isOpen; }
+			set {_isOpen = value; }
+		}
+		
+		public bool isClosed
+		{
+			get	{ return _isClosed; }
+			set {_isClosed = value; }
+		}
+		
+		public ArrayList neighbors
+		{
+			get	{ return _neighbors; }
+			set {_neighbors = value; }
+		}
+		
+		public string toString()
+		{
+			return "{x:" + position.x + ", y:" + position.y + ", f: " + f + "}";
+		}
+}
---
+++ Assets/Scripts/GUI/Points.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Points : MonoBehaviour {
+
+	private int totalScore = 0;
+	public Text countText;
+	public Text totalText;
+
+	[SerializeField]
+	private int increaseScore = 20;
+
+	
+	void Awake () 
+	{
+		InvokeRepeating("AddScore", 0, 3f);
+		SetText ();
+	}
+
+	void AddScore () {
+		totalScore += increaseScore;
+
+		Debug.Log ("Total: " + totalScore);
+		SetText ();
+
+	}
+
+	void SetText ()
+	{
+		countText.text = "+ " + increaseScore.ToString ();
+		totalText.text = "Resource Points: " + totalScore.ToString ();
+	}
+
+}
